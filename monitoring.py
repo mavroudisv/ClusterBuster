a31dc540bc0397d1bd44d541e9b256121e533ed4
mavroudisv <mavroudisv@gmail.com>
2019-03-11 20:11:40 +0000

    do not look for logs when user kills the process

--- monitoring.py
+++ monitoring.py
@@ -8,9 +8,14 @@ from mailer import send_mail
 import common as cm
 
 def read_job_result(jid,jname):
-	filepath = cm.HOME_PATH + jname + ".o" + jid
-	with open(filepath, 'r') as fin:
-		contents = fin.read()
+	contents = ''
+	try:
+		filepath = cm.HOME_PATH + jname + ".o" + jid
+		with open(filepath, 'r') as fin:
+			contents = fin.read()
+		contents = contents.replace("\n","</br>")
+	except:
+		"No file found. Job probably killed by user."
 	return contents
 	
 def event_state_change(id,name,current_state, other=None):
